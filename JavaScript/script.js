8e9edb677a54c53910aa56562816e0fb3f0e5222
Thanu-2002 <mt.didu9988@gmail.com>
2024-10-25 17:00:34 +0530

    add js

---
+++ JavaScript/script.js
@@ -0,0 +1,11 @@
+/* Open the sidebar */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Close the sidebar */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
